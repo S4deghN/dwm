4f765303b9d4c0cb1c878294cbfcb7b540c9f642
Zero <sadegh.nobakhti@gmail.com>
2025-11-21 02:02:44 +0330

    gaps

--- config.h
+++ config.h
@@ -4,7 +4,8 @@
 #include <X11/XF86keysym.h>
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const unsigned int gappx     = 18;       /* gap pixel between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -88,8 +89,7 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_7,                              6)
 	TAGKEYS(                        XK_8,                              7)
 	TAGKEYS(                        XK_9,                              8)
-	{ MODKEY,                       XK_p,              spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return,         spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_x,              spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_t,              spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_a,              spawn,          {.v = (char*[]){ "rofi", "-show", "drun", NULL } } },
     { MODKEY,                       XK_r,              spawn,          {.v = (char*[]){ "st", "-e", "ranger", NULL } } },
@@ -100,7 +100,7 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_p,              incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,              setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,              setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Return,         zoom,           {0} },
+	{ MODKEY,                       XK_s,              zoom,           {0} },
 	{ MODKEY,                       XK_Tab,            view,           {0} },
 	{ MODKEY,                       XK_d,              killclient,     {0} },
 	{ MODKEY,                       XK_f,              setlayout,      {0} },
--- dwm.c
+++ dwm.c
@@ -780,20 +780,22 @@ drawbars(void)
 void
 enternotify(XEvent *e)
 {
-	Client *c;
-	Monitor *m;
-	XCrossingEvent *ev = &e->xcrossing;
+    // Disabled!
 
-	if ((ev->mode != NotifyNormal || ev->detail == NotifyInferior) && ev->window != root)
-		return;
-	c = wintoclient(ev->window);
-	m = c ? c->mon : wintomon(ev->window);
-	if (m != selmon) {
-		unfocus(selmon->sel, 1);
-		selmon = m;
-	} else if (!c || c == selmon->sel)
-		return;
-	focus(c);
+	// Client *c;
+	// Monitor *m;
+	// XCrossingEvent *ev = &e->xcrossing;
+
+	// if ((ev->mode != NotifyNormal || ev->detail == NotifyInferior) && ev->window != root)
+	// 	return;
+	// c = wintoclient(ev->window);
+	// m = c ? c->mon : wintomon(ev->window);
+	// if (m != selmon) {
+	// 	unfocus(selmon->sel, 1);
+	// 	selmon = m;
+	// } else if (!c || c == selmon->sel)
+	// 	return;
+	// focus(c);
 }
 
 void
@@ -1756,28 +1758,31 @@ tagmon(const Arg *arg)
 void
 tile(Monitor *m)
 {
-	unsigned int i, n, h, mw, my, ty;
+	unsigned int i, n, h, mw, my, ty, ns;
 	Client *c;
 
 	for (n = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next), n++);
 	if (n == 0)
 		return;
 
-	if (n > m->nmaster)
+	if (n > m->nmaster) {
 		mw = m->nmaster ? m->ww * m->mfact : 0;
-	else
+		ns = m->nmaster > 0 ? 2 : 1;
+	} else {
 		mw = m->ww;
-	for (i = my = ty = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next), i++)
+		ns = 1;
+	}
+	for(i = 0, my = ty = gappx, c = nexttiled(m->clients); c; c = nexttiled(c->next), i++)
 		if (i < m->nmaster) {
-			h = (m->wh - my) / (MIN(n, m->nmaster) - i);
-			resize(c, m->wx, m->wy + my, mw - (2*c->bw), h - (2*c->bw), 0);
+			h = (m->wh - my) / (MIN(n, m->nmaster) - i) - gappx;
+			resize(c, m->wx + gappx, m->wy + my, mw - (2*c->bw) - gappx*(5-ns)/2, h - (2*c->bw), False);
 			if (my + HEIGHT(c) < m->wh)
-				my += HEIGHT(c);
+				my += HEIGHT(c) + gappx;
 		} else {
-			h = (m->wh - ty) / (n - i);
-			resize(c, m->wx + mw, m->wy + ty, m->ww - mw - (2*c->bw), h - (2*c->bw), 0);
+			h = (m->wh - ty) / (n - i) - gappx;
+			resize(c, m->wx + mw + gappx/ns, m->wy + ty, m->ww - mw - (2*c->bw) - gappx*(5-ns)/2, h - (2*c->bw), False);
 			if (ty + HEIGHT(c) < m->wh)
-				ty += HEIGHT(c);
+				ty += HEIGHT(c) + gappx;
 		}
 }
 
