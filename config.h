cb6feac2a287eb71c52d04c5c0e788ca6b1b2c22
Zero <sadegh.nobakhti@gmail.com>
2025-11-25 07:17:27 +0330

    config

--- config.h
+++ config.h
@@ -13,8 +13,8 @@ static const int showtab            = showtab_auto;        /* Default tab bar sh
 static const int toptab             = True;               /* False means bottom tab bar */
 
 /* appearance */
-static const unsigned int borderpx       = 1;        /* border pixel of windows */
-static const Gap default_gap             = {.isgap = 1, .realgap = 10, .gappx = 15};
+static const unsigned int borderpx       = 2;        /* border pixel of windows */
+static const Gap default_gap             = {.isgap = 1, .realgap = 10, .gappx = 20};
 static const unsigned int snap           = 32;       /* snap pixel */
 static const unsigned int systraypinning = 0;        /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayonleft  = 0;        /* 0: systray in the right corner, >0: systray on left of status text */
